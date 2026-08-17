c172d21c9ee766575a78c13308e1fd0ddd776a94
Kaio Dayvison <kaiodayvison@gmail.com>
2026-08-17 15:15:09 -0300

    style: apply terraform canonical formatting

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -101,16 +101,16 @@ resource "azurerm_log_analytics_workspace" "law" {
 
 # 6. Azure Key Vault (GRC & Compliance Hardening)
 resource "azurerm_key_vault" "kv" {
-  name                        = "kv-${lower(var.project_name)}-${var.environment}"
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = true
-  rbac_authorization_enabled  = true
+  name                          = "kv-${lower(var.project_name)}-${var.environment}"
+  location                      = azurerm_resource_group.rg.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption   = true
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days    = 7
+  purge_protection_enabled      = true
+  rbac_authorization_enabled    = true
   public_network_access_enabled = false
-  sku_name                    = "standard"
+  sku_name                      = "standard"
 
   network_acls {
     bypass         = "AzureServices"
